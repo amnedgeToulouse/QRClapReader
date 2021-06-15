ede2fecaf47107f77c2b5a6db15cc42b972c4d71
amnedge <amnedge@gmail.com>
2021-06-15 18:40:06 +0200

    Remove debug

--- src/app/doBackup/do-backup.component.ts
+++ src/app/doBackup/do-backup.component.ts
@@ -19,8 +19,8 @@ export class DoBackupComponent implements OnInit, OnDestroy {
 
   public renderer: IpcRenderer;
   public remote: Remote;
-  public sourceFolder: string = "F:\\Dropbox\\CameraQRCode\\LecteurElectron\\Test Copy 3 - Copie";
-  public destinations: string[] = ["F:\\Dropbox\\CameraQRCode\\LecteurElectron\\Test Copy 1"];
+  public sourceFolder: string = "";
+  public destinations: string[] = [""];
 
   loading = false;
   faFolderMinus = faFolderMinus;
